8ae92914e56e38c83af08834f4f54deb96a1d3bc
Aleksandr Bobrovitskii <shmoxandco@mail.ru>
2020-11-13 12:46:11 +0300

    + db fix

--- src/main/resources/db/migration/V002__cars.sql
+++ src/main/resources/db/migration/V002__cars.sql
@@ -3,7 +3,8 @@ create table cars
     id  serial not null
         constraint cars_pk
             primary key,
-    model_name varchar
+    model_name varchar,
+    price bigint
 );
 
 INSERT INTO cars (id, model_name, price)
